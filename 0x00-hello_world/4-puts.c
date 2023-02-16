2ff2a2bec31e19e32b2f2681ac1c842666cc66a6
Peteroms <omondipeter934@gmail.com>
2023-02-16 11:41:08 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'first program'
+ *
+ * Return: Always (0) sucess
+ */
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
